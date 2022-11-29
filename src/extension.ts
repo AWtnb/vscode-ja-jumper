07524917a2d4b5ac367c6b323b19ca761501bb4a
awtnb <arikaebanataw@gmail.com>
2022-11-29 22:06:08 +0900

    fix

--- src/extension.ts
+++ src/extension.ts
@@ -6,8 +6,9 @@ const config = vscode.workspace.getConfiguration("ja-jumper");
 const JUMPER = new Jumper(config.get("extras"));
 
 export function activate(context: vscode.ExtensionContext) {
-  context.subscriptions.push(vscode.commands.registerCommand("bnkn.jumpFore", (isSelecting: boolean) => JUMPER.jumpFore(isSelecting)));
-  context.subscriptions.push(vscode.commands.registerCommand("bnkn.jumpBack", (isSelecting: boolean) => JUMPER.jumpBack(isSelecting)));
+  context.subscriptions.push(vscode.commands.registerCommand("ja-jumper.helloJumper", () => console.log("Ja-Jumper was enabled!")));
+  context.subscriptions.push(vscode.commands.registerCommand("ja-jumper.jumpFore", (isSelecting: boolean) => JUMPER.jumpFore(isSelecting)));
+  context.subscriptions.push(vscode.commands.registerCommand("ja-jumper.jumpBack", (isSelecting: boolean) => JUMPER.jumpBack(isSelecting)));
 }
 
 export function deactivate() {}
