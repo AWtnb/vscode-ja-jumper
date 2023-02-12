d449b16a7779743c982be409f9c59c5f6c294d7b
awtnb <arikaebanataw@gmail.com>
2023-02-12 12:00:00 +0900

    load config inside activation

--- src/extension.ts
+++ src/extension.ts
@@ -2,12 +2,12 @@ import * as vscode from "vscode";
 
 import { Jumper } from "./jumper";
 
-const config = vscode.workspace.getConfiguration("ja-jumper");
-const delimiters: string = config.get("delimiters") || "";
-const isGreedy: boolean = config.get("isGreedy") || false;
-const JUMPER = new Jumper(delimiters, isGreedy);
-
 export function activate(context: vscode.ExtensionContext) {
+  const config = vscode.workspace.getConfiguration("ja-jumper");
+  const delimiters: string = config.get("delimiters") || "";
+  const isGreedy: boolean = config.get("isGreedy") || false;
+  const JUMPER = new Jumper(delimiters, isGreedy);
+
   context.subscriptions.push(
     vscode.commands.registerTextEditorCommand("ja-jumper.jumpFore", (editor: vscode.TextEditor) => {
       JUMPER.jumpFore(editor, false);
